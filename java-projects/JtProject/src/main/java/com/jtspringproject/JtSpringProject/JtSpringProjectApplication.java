7099a319944ddb7588c61731387a64e9be251510
kurekako2017 <kurekako2017@gmail.com>
2025-12-27 15:43:05 +0900

    feat: initial workspace structure

---
+++ java-projects/JtProject/src/main/java/com/jtspringproject/JtSpringProject/JtSpringProjectApplication.java
@@ -0,0 +1,15 @@
+package com.jtspringproject.JtSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
+
+@SpringBootApplication(exclude = HibernateJpaAutoConfiguration.class)
+public class JtSpringProjectApplication {
+
+	public static void main(String[] args) {
+		System.out.println("test1");
+		SpringApplication.run(JtSpringProjectApplication.class, args);
+	}
+
+}
